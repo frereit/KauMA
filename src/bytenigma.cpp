17b63acaf8f4b25906b1537fd65cbb7ba62a42a6
Frederik Reiter <s211268@student.dhbw-mannheim.de>
2023-10-16 15:02:07 +0200

    feat(bytenigma): Implement `forward_pass`
    Add `forward_pass` implementation
    Add `forward_pass` test

--- src/bytenigma.cpp
+++ src/bytenigma.cpp
@@ -21,8 +21,12 @@ Bytenigma::Bytenigma::process_bytes(const std::vector<std::uint8_t> &input) {
 }
 
 std::uint8_t Bytenigma::Bytenigma::forward_pass(std::uint8_t input) {
-  (void)input;
-  return std::uint8_t();
+  for (std::size_t i = 0; i < m_rotors.size(); ++i) {
+    std::vector<std::uint8_t> rotor = m_rotors[i];
+    std::uint8_t position = m_rotor_positions[i];
+    input = rotor.at((input + position) % rotor.size());
+  }
+  return input;
 }
 
 std::uint8_t Bytenigma::Bytenigma::backward_pass(std::uint8_t input) {
@@ -33,3 +37,23 @@ std::uint8_t Bytenigma::Bytenigma::backward_pass(std::uint8_t input) {
 void Bytenigma::Bytenigma::turn_rotor(const std::uint8_t &index) {
   (void)index;
 }
+
+#ifdef TEST
+#include "doctest.h"
+
+TEST_CASE("test bytenigma forward pass") {
+  auto rotors = std::vector<std::vector<std::uint8_t>>();
+  for (size_t i = 0; i < 3; ++i) {
+    auto rotor = std::vector<std::uint8_t>();
+    for (std::size_t j = 0; j < 256; ++j) {
+      rotor.push_back((j + 1) % 256); // 0->1, 1->2 and so on
+    }
+    rotors.push_back(rotor);
+  }
+  Bytenigma::Bytenigma enigma = Bytenigma::Bytenigma(rotors);
+  CHECK(enigma.forward_pass(0) == 3);
+  CHECK(enigma.forward_pass(50) == 53);
+  CHECK(enigma.forward_pass(255) == 2);
+}
+
+#endif

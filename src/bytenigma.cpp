789f13f10baf1c089fb8d3a243822608a1110dbf
Frederik Reiter <s211268@student.dhbw-mannheim.de>
2023-10-16 14:59:43 +0200

    feat(bytenigma): Add implementation of public APIs
    The underlying private methods are still unimplemented.

--- src/bytenigma.cpp
+++ src/bytenigma.cpp
@@ -4,14 +4,20 @@
 #include "bytenigma.hpp"
 
 std::uint8_t Bytenigma::Bytenigma::process_byte(std::uint8_t input) {
-  (void)input;
-  return std::uint8_t();
+  std::uint8_t forward = this->forward_pass(input);
+  std::uint8_t complement = ~forward;
+  std::uint8_t backward = this->backward_pass(complement);
+  this->turn_rotor(0);
+  return backward;
 }
 
 std::vector<std::uint8_t>
 Bytenigma::Bytenigma::process_bytes(const std::vector<std::uint8_t> &input) {
-  (void)input;
-  return std::vector<std::uint8_t>();
+  std::vector<std::uint8_t> output;
+  for (std::uint8_t byte : input) {
+    output.push_back(this->process_byte(byte));
+  }
+  return output;
 }
 
 std::uint8_t Bytenigma::Bytenigma::forward_pass(std::uint8_t input) {

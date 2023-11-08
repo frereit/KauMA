45e745ffb4780117808686573b6f59d44405f8b4
Frederik Reiter <s211268@student.dhbw-mannheim.de>
2023-11-08 18:37:02 +0100

    feat(padding_oracle): Add glue code for padding-oracle-attack

--- src/glue.cpp
+++ src/glue.cpp
@@ -1,12 +1,14 @@
 #include <algorithm>
 #include <cstdint>
 #include <nlohmann/json.hpp>
+#include <string>
 #include <vector>
 
 #include "bytenigma.hpp"
 #include "cppcodec/base64_rfc4648.hpp"
 #include "glue.hpp"
 #include "noop.hpp"
+#include "padding_oracle/attack.hpp"
 #include "padding_oracle/server.hpp"
 
 using json = nlohmann::json;
@@ -36,7 +38,8 @@ extern const std::map<std::string, Glue::glue_function> Glue::ACTIONS = {
        json output = {{"output", cppcodec::base64_rfc4648::encode(raw_output)}};
        return output;
      }},
-    {"padding-oracle-server", [](const json &input) {
+    {"padding-oracle-server",
+     [](const json &input) {
        PaddingOracle::block key;
        std::vector<std::uint8_t> raw_key =
            cppcodec::base64_rfc4648::decode(input["key"].get<std::string>());
@@ -48,6 +51,23 @@ extern const std::map<std::string, Glue::glue_function> Glue::ACTIONS = {
        PaddingOracle::XORCBCServer server{key, port};
        server.loop_forever();
        return json();
+     }},
+    {"padding-oracle-attack", [](const json &input) {
+       PaddingOracle::block iv;
+       std::vector<std::uint8_t> raw_iv =
+           cppcodec::base64_rfc4648::decode(input["iv"].get<std::string>());
+       if (raw_iv.size() != iv.size()) {
+         throw std::runtime_error("Invalid iv size!");
+       }
+       std::copy_n(raw_iv.begin(), iv.size(), iv.begin());
+       std::vector<std::uint8_t> ciphertext = cppcodec::base64_rfc4648::decode(
+           input["ciphertext"].get<std::string>());
+       std::string hostname = input["hostname"].get<std::string>();
+       std::uint16_t port = input["port"].get<std::uint16_t>();
+       PaddingOracle::Attack attack{hostname, port};
+       std::vector<std::uint8_t> plaintext =
+           attack.recover_plaintext(iv, ciphertext);
+       return json({{"plaintext", cppcodec::base64_rfc4648::encode(plaintext)}});
      }}};
 
 /// @brief executes a specific action by parsing the arguments from the JSON and

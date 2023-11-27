2f4a1367f5d068e4953e456848382031d2ccfbdb
Frederik Reiter <s211268@student.dhbw-mannheim.de>
2023-11-27 14:52:40 +0100

    feat(gcm): Add streamable GHASH implementation

--- include/gcm/ghash.hpp
+++ include/gcm/ghash.hpp
@@ -1,9 +1,59 @@
 #pragma once
 #include <cstdint>
+#include <deque>
 #include <vector>
 
+#include "gcm/polynomial.hpp"
+
 namespace GCM {
+
+/// @brief A streamable GHASH implementation
+class GHASH {
+
+  static constexpr std::uint64_t BLOCK_SIZE = 16;
+
+public:
+  /// @brief Start a new GHASH computation
+  /// @param associated_data associated data must be known when beginning the
+  /// computation
+  /// @param auth_key the GHASH auth key
+  GHASH(std::vector<std::uint8_t> associated_data,
+        std::vector<std::uint8_t> auth_key);
+
+  /// @brief add ciphertext to the GHASH
+  /// @param ciphertext ciphertext of any length
+  void update(std::vector<std::uint8_t> ciphertext);
+
+  /// @brief finalize the computation and return the result
+  /// @return the authentication tag for all inserted data
+  std::vector<std::uint8_t> finalize();
+
+private:
+  /// @brief pad the current ciphertext block with zeros and add it to the tag
+  void pad_current_block();
+
+  /// @brief the current auth tag
+  GCM::Polynomial m_auth_tag;
+
+  const GCM::Polynomial m_auth_key;
+  const std::uint64_t m_associated_data_bitlength;
+  std::uint64_t m_ciphertext_bitlength;
+
+  /// @brief the ciphertext buffer contains all "remaining" ciphertext data. Its
+  /// length is always less than GCM::GHASH::BLOCK_SIZE. It is a std::deque
+  /// because we insert at the end and remove from the beginning.
+  std::deque<std::uint8_t> m_ciphertext_buffer;
+  bool m_finalized;
+};
+
+/// @brief compute an auth tag for a given ciphertext
+/// @param ciphertext the ciphertext
+/// @param associated_data the associated data
+/// @param key the auth key to use
+/// @return the auth tag
+/// @note this is a convience function. For streamable hashing, use the
+/// GCM::GHASH class.
 std::vector<std::uint8_t> ghash(std::vector<std::uint8_t> ciphertext,
                                 std::vector<std::uint8_t> associated_data,
                                 std::vector<std::uint8_t> key);
-}
+} // namespace GCM
--- src/gcm/ghash.cpp
+++ src/gcm/ghash.cpp
@@ -1,39 +1,76 @@
+#include <cassert>
 #include <cstdint>
+#include <deque>
+#include <stdexcept>
 #include <vector>
 
 #include "bytemanipulation.hpp"
 #include "gcm/ghash.hpp"
 #include "gcm/polynomial.hpp"
 
+GCM::GHASH::GHASH(std::vector<std::uint8_t> associated_data,
+                  std::vector<std::uint8_t> auth_key)
+    : m_auth_tag(GCM::Polynomial(0)),
+      m_auth_key(GCM::Polynomial::from_gcm_bytes(auth_key)),
+      m_associated_data_bitlength(associated_data.size() * 8),
+      m_ciphertext_bitlength(0),
+      m_ciphertext_buffer(std::deque<std::uint8_t>(0)), m_finalized(false) {
+  // Add associated data to the beginning of the "stream", padded with 0s
+  this->update(associated_data);
+  this->pad_current_block();
+
+  // Set the ciphertext bitlength back to 0. This was updated by the call to
+  // "update".
+  this->m_ciphertext_bitlength = 0;
+}
+
+void GCM::GHASH::update(std::vector<std::uint8_t> ciphertext) {
+  if (m_finalized) {
+    throw std::runtime_error("Cannot update finalized GHASH.");
+  }
+  m_ciphertext_buffer.insert(m_ciphertext_buffer.end(), ciphertext.begin(),
+                             ciphertext.end());
+  while (m_ciphertext_buffer.size() >= GCM::GHASH::BLOCK_SIZE) {
+    std::vector<std::uint8_t> block(m_ciphertext_buffer.begin(),
+                                    m_ciphertext_buffer.begin() +
+                                        GCM::GHASH::BLOCK_SIZE);
+    m_ciphertext_buffer.erase(m_ciphertext_buffer.begin(),
+                              m_ciphertext_buffer.begin() +
+                                  GCM::GHASH::BLOCK_SIZE);
+    m_auth_tag += GCM::Polynomial::from_gcm_bytes(block);
+    m_auth_tag *= m_auth_key;
+    m_ciphertext_bitlength += block.size() * 8;
+  }
+}
+
+std::vector<std::uint8_t> GCM::GHASH::finalize() {
+  // Pad the remaining ciphertext
+  this->pad_current_block();
+
+  // Create the length field
+  std::vector<std::uint8_t> length;
+  ByteManipulation::append_as_bytes<std::uint64_t>(m_associated_data_bitlength,
+                                                   std::endian::big, length);
+  ByteManipulation::append_as_bytes<std::uint64_t>(m_ciphertext_bitlength,
+                                                   std::endian::big, length);
+  this->update(length);
+  this->m_finalized = true;
+  return m_auth_tag.to_gcm_bytes();
+}
+
+void GCM::GHASH::pad_current_block() {
+  std::uint64_t padding_length =
+      GCM::GHASH::BLOCK_SIZE - m_ciphertext_buffer.size();
+  if (padding_length == GCM::GHASH::BLOCK_SIZE)
+    return;
+  this->update(std::vector<std::uint8_t>(padding_length));
+  m_ciphertext_bitlength -= padding_length * 8;
+}
+
 std::vector<std::uint8_t> GCM::ghash(std::vector<std::uint8_t> ciphertext,
                                      std::vector<std::uint8_t> associated_data,
                                      std::vector<std::uint8_t> key) {
-  GCM::Polynomial auth_tag = GCM::Polynomial(std::bitset<128>(0));
-  GCM::Polynomial auth_key = GCM::Polynomial::from_gcm_bytes(key);
-
-  for (const auto &v : {associated_data, ciphertext}) {
-    for (std::size_t i = 0; i < v.size(); i += key.size()) {
-      std::vector<std::uint8_t>::const_iterator end;
-      if (i + key.size() >= v.size()) {
-        end = v.end();
-      } else {
-        end = v.begin() + i + key.size();
-      }
-
-      std::vector<std::uint8_t> block(v.begin() + i, end);
-      block.resize(key.size(), 0);
-      auth_tag += GCM::Polynomial::from_gcm_bytes(block);
-      auth_tag *= auth_key;
-    }
-  }
-
-  std::vector<std::uint8_t> length;
-  ByteManipulation::append_as_bytes<std::uint64_t>(associated_data.size() * 8,
-                                                   std::endian::big, length);
-  ByteManipulation::append_as_bytes<std::uint64_t>(ciphertext.size() * 8,
-                                                   std::endian::big, length);
-  auth_tag += GCM::Polynomial::from_gcm_bytes(length);
-  auth_tag *= auth_key;
-
-  return auth_tag.to_gcm_bytes();
+  GCM::GHASH hasher = GCM::GHASH(associated_data, key);
+  hasher.update(ciphertext);
+  return hasher.finalize();
 }

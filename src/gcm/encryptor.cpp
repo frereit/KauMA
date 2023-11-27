243eec9fcf33b215458cc33ab4b9749fad25a5ad
Frederik Reiter <s211268@student.dhbw-mannheim.de>
2023-11-27 15:34:29 +0100

    feat(gcm): Add streamable GCM implementation

--- src/gcm/encryptor.cpp
+++ src/gcm/encryptor.cpp
@@ -12,9 +12,11 @@
 #include "gcm/ghash.hpp"
 #include "gcm/polynomial.hpp"
 
-GCM::Encryptor::Encryptor(std::unique_ptr<Botan::BlockCipher> cipher,
+GCM::Encryptor::Encryptor(std::vector<std::uint8_t> associated_data,
+                          std::unique_ptr<Botan::BlockCipher> cipher,
                           const std::vector<std::uint8_t> &nonce)
-    : m_cipher(std::move(cipher)) {
+    : m_cipher(std::move(cipher)), m_y(1),
+      m_hasher(associated_data, this->h()) {
   // SAFETY: This requirement is used in GCM::Encryptor::ghash.
   assert(m_cipher->block_size() == 16 &&
          "GCM is only implemented for ciphers with 16-byte blocks.");
@@ -25,15 +27,6 @@ GCM::Encryptor::Encryptor(std::unique_ptr<Botan::BlockCipher> cipher,
   } else {
     m_y0 = GCM::ghash(nonce, {}, this->h());
   }
-}
-
-GCM::EncryptionResult GCM::Encryptor::encrypt_and_authenticate(
-    std::vector<std::uint8_t> plaintext,
-    std::vector<std::uint8_t> associated_data) {
-  std::vector<std::uint8_t> ciphertext = this->encrypt(plaintext);
-  std::vector<std::uint8_t> auth_tag =
-      this->authenticate(ciphertext, associated_data);
-  return {ciphertext, associated_data, auth_tag};
 }
 
 std::vector<std::uint8_t> GCM::Encryptor::y0() { return this->m_y0; }
@@ -55,26 +48,23 @@ std::vector<std::uint8_t> GCM::Encryptor::y_block(std::uint32_t y) {
 }
 
 std::vector<std::uint8_t>
-GCM::Encryptor::encrypt(std::vector<std::uint8_t> plaintext) {
-  std::vector<std::uint8_t> ciphertext, keystream;
-  std::uint32_t y = 1;
+GCM::Encryptor::update(std::vector<std::uint8_t> plaintext) {
+  std::vector<std::uint8_t> ciphertext;
   for (const std::uint8_t &pt : plaintext) {
-    if (keystream.size() == 0) {
+    if (m_keystream.size() == 0) {
       // No bytes left in the keystream so we need to encrypt a new block
-      keystream = this->y_block(y++);
-      this->m_cipher->encrypt(keystream);
+      m_keystream = this->y_block(m_y++);
+      this->m_cipher->encrypt(m_keystream);
     }
-    ciphertext.push_back(pt ^ keystream.at(0));
-    keystream.erase(keystream.begin());
+    ciphertext.push_back(pt ^ m_keystream.at(0));
+    m_keystream.erase(m_keystream.begin());
   }
+  m_hasher.update(ciphertext);
   return ciphertext;
 }
 
-std::vector<std::uint8_t>
-GCM::Encryptor::authenticate(std::vector<std::uint8_t> ciphertext,
-                             std::vector<std::uint8_t> associated_data) {
-  std::vector<std::uint8_t> auth_tag =
-      GCM::ghash(ciphertext, associated_data, this->h());
+std::vector<std::uint8_t> GCM::Encryptor::finalize() {
+  std::vector<std::uint8_t> auth_tag = m_hasher.finalize();
 
   std::vector<std::uint8_t> auth_tag_mask = this->y0();
   this->m_cipher->encrypt(auth_tag_mask);
@@ -95,7 +85,7 @@ TEST_CASE("test counter mode block generator with 12 byte nonce") {
   aes->set_key(key);
   std::vector<std::uint8_t> nonce =
       Botan::hex_decode("aa1d5a0aa1ea09f6ff91e534");
-  GCM::Encryptor e = GCM::Encryptor(std::move(aes), nonce);
+  GCM::Encryptor e = GCM::Encryptor({}, std::move(aes), nonce);
   CHECK(Botan::hex_encode(e.y_block(0)) == "AA1D5A0AA1EA09F6FF91E53400000001");
   CHECK(Botan::hex_encode(e.y_block(0xc478)) ==
         "AA1D5A0AA1EA09F6FF91E5340000C479");
@@ -111,7 +101,7 @@ TEST_CASE("test counter mode block generator with 8 byte nonce") {
   std::vector<std::uint8_t> nonce = Botan::hex_decode("cafebabefacedbad");
   auto aes = Botan::BlockCipher::create_or_throw("AES-128");
   aes->set_key(key);
-  GCM::Encryptor e = GCM::Encryptor(std::move(aes), nonce);
+  GCM::Encryptor e = GCM::Encryptor({}, std::move(aes), nonce);
   CHECK(Botan::hex_encode(e.y_block(0)) == "C43A83C4C4BADEC4354CA984DB252F7D");
   CHECK(Botan::hex_encode(e.y_block(1)) == "C43A83C4C4BADEC4354CA984DB252F7E");
   CHECK(Botan::hex_encode(e.y_block(3)) == "C43A83C4C4BADEC4354CA984DB252F80");
